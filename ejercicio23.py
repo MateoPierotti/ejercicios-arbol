e2b88730240a956c71df2ad1079fb2aa47999182
MateoPierotti <127799431+MateoPierotti@users.noreply.github.com>
2023-10-10 20:14:09 -0300

    Update ejercicio23.py

--- ejercicio23.py
+++ ejercicio23.py
@@ -68,7 +68,7 @@ def agregarCapturado(criaturas):
 def buscarPorConcidencia(criaturas,quien):
     criaturas.search_by_quien(quien)
 
-#! d. determinar los 3 héroes o dioses que derrotaron mayor cantidad de Criaturas;
+
 
 
 print("Punto B")
@@ -80,6 +80,21 @@ agregarDescripcion(criaturas, quien= hola, mensaje=hola1)
 print("Punto C")
 print('informacion de Talos')
 infoDeTalos(criaturas)
+
+#! d. determinar los 3 héroes o dioses que derrotaron mayor cantidad de Criaturas;
+print("Punto D")
+dic_ranking = {}
+criaturas.inorden_ranking(dic_ranking)
+# print(dic_ranking)
+
+def order_por(item):
+    # print(item)
+    return item[1]
+
+ordenados = list(dic_ranking.items())
+ordenados.sort(key=order_por, reverse=True)
+print(ordenados[1:4])
+
 # e. listar las Criaturas derrotadas por Heracles;
 print("Punto E")
 print("Criaturas derrotadas por Heracles")
